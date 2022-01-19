96f2eba40398319ed5b54cd9e12dad6236a5f98c
xiangsu <xiangsu@gaoding.com>
2022-01-19 15:30:40 +0800

    tijaio

--- vue2canvaindex/src/main.js
+++ vue2canvaindex/src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import "./style/index.less";
 import "element-ui/lib/theme-chalk/index.css";
 import "./lib/element";
+// import tween from '../../package/tween'
+// console.log(tween)
 Vue.config.productionTip = false;
 new Vue({
   router,
